ae81bccbbdc5b9c98c03fc49eb8d2418b88739e0
Quy Trân <quyt40247@gmail.com>
2024-04-14 18:40:57 +0700

    Build screens

---
+++ lib/consts/theme_data.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shopsmart_users_en/consts/app_colors.dart';
+
+class Styles {
+  static ThemeData themeData(
+      {required bool isDarkTheme, required BuildContext context}) {
+    return ThemeData(
+      scaffoldBackgroundColor: isDarkTheme
+          ? AppColors.darkScaffoldColor
+          : AppColors.lightScaffoldColor,
+      cardColor: isDarkTheme
+          ? const Color.fromARGB(255, 13, 6, 37)
+          : AppColors.lightCardColor,
+      brightness: isDarkTheme ? Brightness.dark : Brightness.light,
+      appBarTheme: AppBarTheme(
+        backgroundColor: isDarkTheme
+            ? AppColors.darkScaffoldColor
+            : AppColors.lightScaffoldColor,
+        elevation: 0,
+        titleTextStyle: TextStyle(
+          color: isDarkTheme ? Colors.white : Colors.black,
+        ),
+      ),
+    );
+  }
+}
